916c81c4b8808c360124dbe9dba8d4fec3c58f59
sugarware <46047287+sugarware@users.noreply.github.com>
2026-08-11 05:49:04 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'sleeping-kindle-v0.8d-pwa-1';
+
+const LOCAL_ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './jszip.min.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png',
+  './icons/apple-touch-icon.png',
+  './icons/favicon-32.png',
+  './icons/favicon-16.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(LOCAL_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const networkFetch = fetch(event.request).then(response => {
+        if (response && (response.ok || response.type === 'opaque')) {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      });
+
+      if (event.request.mode === 'navigate') {
+        return networkFetch.catch(() => cached || caches.match('./index.html'));
+      }
+      return cached || networkFetch;
+    })
+  );
+});
